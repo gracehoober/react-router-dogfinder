a749368065d44847277d0fe3361b8d540b8c964a
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-10-02 15:15:47 -0700

    nav bar completed

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 function Nav({ names }) {
   console.log("names in NAV", names);
+  return(
+    <>
+      <nav>
+        {names.map(name => <NavLink to={`/dogs/${name}`}>{name}</NavLink>)}
+      </nav>
+    </>
+  )
 }
 
 export default Nav;
